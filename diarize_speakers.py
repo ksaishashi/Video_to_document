457c854d3edfd17e6b489ca6bda10410aeda0364
Sai Shashank Karnati <ksaishashank8@gmail.com>
2024-11-17 10:33:45 +0530

    Update diarize_speakers.py

--- diarize_speakers.py
+++ diarize_speakers.py
@@ -1,24 +1,9 @@
 
-# from pyannote.audio import Pipeline
-
-# def diarize_audio(audio_path: str, transcription: dict) -> list:
-#     """Perform speaker diarization."""
-#     pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1", use_auth_token="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")
-#     diarization = pipeline(audio_path)
-#     diarized_segments = []
-#     for segment, _, speaker in diarization.itertracks(yield_label=True):
-#         diarized_segments.append({
-#             "speaker": speaker,
-#             "start": segment.start,
-#             "end": segment.end,
-#             "text": transcription["text"]  # Timestamp alignment can be improved
-#         })
-#     return diarized_segments
 
 from pyannote.audio import Pipeline
 
 def diarize_audio(audio_path: str, transcription: dict) -> list:
-    pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1", use_auth_token="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")
+    pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1", use_auth_token="YOUR_huggingface_API")
     diarization = pipeline(audio_path,num_speakers=4)
 
     diarized_segments = []
